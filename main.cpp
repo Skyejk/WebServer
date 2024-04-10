ae8eb5cbed64c066c4ae92b013de83a91ee6fb9c
skyejk <tokugava_norimura@mail.ru>
2024-04-11 00:02:01 +0300

    end

--- main.cpp
+++ main.cpp
@@ -7,8 +7,22 @@ using server::app;
 using server::Request;
 using server::Response;
 
-int main (int argc, const char * argv[]) {
-
+int main(int argc, const char * argv[]) {
+    
+    auto method = [](Request test, Response res) {
+        res.raw("Hello my frend " + test.query()["t"]);
+        // res.exception("it's the end of the world!");
+        return res;
+    };
+    
+    app()
+    // .debug(false)
+    .addListen("127.0.0.1", 80)
+    .registerHandler("POST:/", method)
+    .registerHandler("GET:/test/getInfo", method)
+    .run();
+    
+    // insert code here...
     std::cout << "Hello, World!\n";
     return 0;
 }
